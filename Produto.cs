a4a5da80b9a0829fd6dc4eaa3194f0f1ce07e183
Jorge-Marcelo <jorge3258@hotmail.com>
2024-05-22 16:19:30 -0300

    Primeiro Commit

---
+++ Produto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_361
+{
+ internal class Produto
+ {
+
+public int Codigo { get; set; }
+public string Nome { get; set; }
+public int Quantidade { get; set; }
+public double ValorCompra { get; set; }
+public double ValorVenda { get; set; }
+
+public Produto(
+int codigo, string nome, int quantidade, 
+double valorCompra, double valorVenda )
+{
+   Codigo = codigo;
+   Nome = nome;
+   Quantidade = quantidade;
+   ValorCompra = valorCompra;
+   ValorVenda = valorVenda;
+}
+
+
+ public void ExibirProduto(){
+   Console.WriteLine($"Codigo do Produto: {Codigo}");
+   Console.WriteLine($"Nome do Produto: {Nome}");
+   Console.WriteLine($"Quantidade: {Quantidade}");
+   Console.WriteLine($"Valor de Compra: {ValorCompra}");
+   Console.WriteLine($"Valor de Venda: {ValorVenda}");
+   Console.Write("");
+
+
+  }
+
+ }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_361
+{
+ internal class Program
+ {
+  static void Main(string[] args)
+  {
+
+List<Produto> produtos = new List<Produto>();
+
+Console.WriteLine("CADASTRO DE PRODUTOS");
+Console.WriteLine("--------------------------------------");
+
+
+   for (int iterador = 0; iterador < 30; iterador++){
+Console.Write($"Digite o  nome do {iterador + 1}° produto: ");
+string nome = Console.ReadLine();
+
+Console.Write("Digite o codigo do produto: ");
+int codigo = int.Parse(Console.ReadLine());
+
+Console.Write("Digite a quantidade: ");
+int quantidade = int.Parse(Console.ReadLine());
+
+Console.Write("Digite no valor de compra: ");
+double valorCompra = double.Parse(Console.ReadLine());
+
+Console.Write("Digite no valor de venda: ");
+double valorVenda = double.Parse(Console.ReadLine());
+
+Console.WriteLine("--------------------------------------");
+
+produtos.Add(new Produto(codigo, nome, quantidade, valorCompra, valorVenda));
+
+
+
+Console.WriteLine("ESCOLHA UMA OPÇÃO");
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("1 - Mostrar Produto ");
+Console.WriteLine("2 - Sair ");
+Console.Write("\nOpção: ");
+int op = int.Parse(Console.ReadLine());
+
+Console.WriteLine("--------------------------------------");
+
+Console.WriteLine("");
+
+
+    switch (op)
+{
+case 1: 
+foreach (Produto produto in produtos) {
+produto.ExibirProduto();
+Console.WriteLine("--------------------------------------");
+}
+break;
+case 2: System.Environment.Exit(0);
+      break;
+
+}
+   }
+
+  }
+ }
+}
